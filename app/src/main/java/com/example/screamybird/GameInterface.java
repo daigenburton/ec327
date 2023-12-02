97d67dbdf4ad98ff6648afef2832836839762b78
kliu3816 <kliu3816@bu.edu>
2023-12-02 18:11:44 -0500

    d

--- app/src/main/java/com/example/screamybird/GameInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.screamybird;
-
-import android.graphics.Canvas;
-
-public interface GameInterface {
-    public void draw(Canvas canvas);
-    public void update();
-}
---
+++ app/src/main/java/com/example/screamybird/PlayerState.java
@@ -0,0 +1,9 @@
+package com.example.screamybird;
+
+public class PlayerState {
+    public enum State{
+        moving
+    }
+
+    private Player player
+}
